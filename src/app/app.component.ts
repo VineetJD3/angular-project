9254f8d77b13cf2ce43502c47208c021adc2d171
Vineet Dsouza <dsouzavineet3@gmail.com>
2017-07-20 20:46:41 +0530

    Data Binding, Event Binding with custom tags

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
+import { HomeComponent } from './home/home.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  entryComponents: [HomeComponent]
 })
 export class AppComponent {
-  title = 'app works!';
+  title = 'Angular Project';
+
+  mainNinja = {
+    name : "Saurabh",
+    belt : "Black"
+  }
+
+  yell(e){
+    alert("I am Yelling...")
+    console.log(e)
+  }
 }
---
+++ src/app/home/home.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+
+@Component({
+  inputs: ["mainNinja"],
+  outputs: ["onYell"],
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css'],
+})
+export class HomeComponent implements OnInit {
+
+  hometitle = "Welcome to the Directory";
+  inputValue: string = "This is Input 1";
+  isRequired:boolean = false;
+  onYell = new EventEmitter();
+
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  alertMe(value){
+    alert(value);
+  }
+
+  ninja = {
+    name : "Saurabh",
+    belt : "Black"
+  }
+
+  yell(e){
+    this.onYell.emit(e);
+  }
+
+}
